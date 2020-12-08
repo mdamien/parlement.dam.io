5173fba5249617b52c23acc1cd724cd26157b7fa
Damien <damien@dam.io>
2020-12-08 11:26:08 +0100

    remove hack

--- config/settings.py
+++ config/settings.py
@@ -24,20 +24,3 @@ DATABASES['db_ns'] = {
     'USER': 'cpc',
     'PASSWORD': 'password',
 }
-
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': False,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
